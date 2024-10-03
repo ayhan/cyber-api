ac6dc68599389f7de506ae1f2cbea6634e845474
Ayhan Kırıkcı <ayhankirikci2264@gmail.com>
2024-10-04 00:38:26 +0300

    feat: vulnerability CRUD add

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const app = express();
 const authRoutes = require("./routes/authRoutes");
+const vulnerabilityRoutes = require("./routes/vulnerabilityRoutes");
 
 app.use(express.json());
 
 // Routes
-app.use("/auth", authRoutes);
+app.use("/api/auth", authRoutes);
+app.use("/api/", vulnerabilityRoutes);
 
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
--- middlewares/authMiddleware.js
+++ middlewares/authMiddleware.js
@@ -2,13 +2,11 @@ const jwt = require("jsonwebtoken");
 const { jwtSecret } = require("../config/config");
 
 module.exports = function (req, res, next) {
-  const token = req.headers["authorization"];
+  const token = req.headers["authorization"]?.split(" ")[1]; // Split 'Bearer'
 
   if (!token) return res.status(403).json({ error: "Token is required" });
 
-  const bearerToken = token.split(" ")[1]; // Split 'Bearer'
-
-  jwt.verify(bearerToken, jwtSecret, (err, decoded) => {
+  jwt.verify(token, jwtSecret, (err, decoded) => {
     if (err) return res.status(403).json({ error: "Invalid token" });
     req.user = decoded;
     next();
