88e1cd0a6124a3cde446e91529fbf88aa46c7825
Ayhan Kırıkcı <ayhankirikci2264@gmail.com>
2024-10-09 01:02:16 +0300

    chore: sample local connection add

--- config/db.js
+++ config/db.js
@@ -1,6 +1,15 @@
 const { Pool } = require("pg");
 const { postgreUrl } = require("../config/config");
 
+// local connection
+// const pool = new Pool({
+//   user: "ayhan",
+//   host: "localhost",
+//   database: "postgres",
+//   password: "pass",
+//   port: 5432,
+// });
+
 const pool = new Pool({
   connectionString: postgreUrl,
 });
